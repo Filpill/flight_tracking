ff4f49b6fac0b3aebe423307d6f8fd460a56d9bf
Filpill <filiplivancic@gmail.com>
2024-05-10 15:12:43 +0000

    updated bash script to new folder structure

--- animate/mkvideo.sh
+++ animate/mkvideo.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-#$1 Specifies the Date of the folder to be accessed - E.g. '2024-08-24'
-#$2 Specifies the Region of the folder to be accessed - E.g. 'us' or 'eu'
+# $1 Specifies the Date of the folder to be accessed - E.g. '2024-08-24'
+# $2 Specifies the Image Folder To Be Accessed - E.g. 'scatter' or 'quiver' or 'contourf'
+# Example of how to run bash script: $(./mkvideo '2024-08-24' 'scatter')
 
 base_folder="$(dirname $(pwd))"
 output="$base_folder/animate/videos"
-image_data="$base_folder/data/get_states/$1/images/$2"
+image_data="$base_folder/data/get_states/$1/$2"
 
-ffmpeg -framerate 24 -pattern_type glob -i "${image_data}/*.png" $output/$2_movements_$1.mp4
+ffmpeg -framerate 24 -pattern_type glob -i "${image_data}/*.png" $output/$1_$2_movements.mp4
